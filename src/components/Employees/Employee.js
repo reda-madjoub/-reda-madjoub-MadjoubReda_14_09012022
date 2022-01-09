1ab2f10f0c9b3570aac74ff4b8fd5665ade25e51
Reda M <reda.madjoub@gmail.com>
2022-01-09 17:49:35 +0100

    initialize project with react

---
+++ src/components/Employees/Employee.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Employee = () => {
+    return (
+        <>
+            <div id="employee-div" class="container">
+                <h1>Current Employees</h1>
+                <table id="employee-table" class="display"></table>
+                <a href="index.html">Home</a>
+            </div>
+        </>
+    )
+}
+
+export default Employee
---
+++ src/components/Home/Home.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+    return (
+        <>
+        <div class="title">
+            <h1>HRnet</h1>
+        </div>
+        <div class="container">
+            <a href="employee-list.html">View Current Employees</a>
+            <h2>Create Employee</h2>
+            <form action="#" id="create-employee">
+                <label for="first-name">First Name</label>
+                <input type="text" id="first-name" />
+
+                <label for="last-name">Last Name</label>
+                <input type="text" id="last-name" />
+
+                <label for="date-of-birth">Date of Birth</label>
+                <input id="date-of-birth" type="text" />
+
+                <label for="start-date">Start Date</label>
+                <input id="start-date" type="text" />
+
+                <fieldset class="address">
+                    <legend>Address</legend>
+
+                    <label for="street">Street</label>
+                    <input id="street" type="text" />
+
+                    <label for="city">City</label>
+                    <input id="city" type="text" />
+
+                    <label for="state">State</label>
+                    <select name="state" id="state"></select>
+
+                    <label for="zip-code">Zip Code</label>
+                    <input id="zip-code" type="number" />
+                </fieldset>
+
+                <label for="department">Department</label>
+                <select name="department" id="department">
+                    <option>Sales</option>
+                    <option>Marketing</option>
+                    <option>Engineering</option>
+                    <option>Human Resources</option>
+                    <option>Legal</option>
+                </select>
+            </form>
+
+            <button onclick="saveEmployee()">Save</button>
+        </div>
+        <div id="confirmation" class="modal">Employee Created!</div>
+        </>
+    )
+}
+
+export default Home
