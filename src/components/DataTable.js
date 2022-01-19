2e533a27db70dac6ef2da9fefc185211e1b22103
Reda M <reda.madjoub@gmail.com>
2022-01-19 03:02:20 +0100

    update version modale-plugin

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -4,8 +4,6 @@ import { Labels, exampleData } from "../constant/data"
 import {Context} from '../utils/context/index'
 
 export const DataTable = () => {
-    console.log(exampleData);
     const { employeeList } = useContext(Context)
-    console.log(employeeList);
     return <DataTables labels={Labels} data={employeeList}/>
 }
